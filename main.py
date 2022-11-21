68894764fed3120efc9b76a2d7f7f71ffd0d1d73
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-11-22 01:35:38 +0200

    class methods

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import csv
+
 class Item:
     """
     Defines a class Item
     """
     # class attribute
     pay_rate = 0.2 # discount after buying 20% of the product
-    
+    all = []
     def __init__(self, name: str, price: float, quantity = 0):
         # Run validation to the recieved arguments
         assert price >= 0, f"price {price} is not greater than Zero"
@@ -14,21 +16,26 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        # Actions to execute
+        Item.all.append(self)
 
     def calculate_total_price(self):
-        """
-        The method calculate the total of quantity
-
-        args:
-        
-        return: i
-        """
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            print(item)
 
 
-item1 = Item("Phone", 100, 4)
-item2 = Item("Laptop", 3000, 6)
+    def __repr__(self):
+        return f"Item ('{self.name}','{self.price}','{self.quantity}')"
 
-print(Item.pay_rate)
-print(item1.pay_rate)
-print(item2.pay_rate)
+print(Item.all)
